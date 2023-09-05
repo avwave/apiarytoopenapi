9b04fe956ec01c2f28c7e2029ba09fa45f3989fc
Ray Arvin Rimorin <ray@fbmsoftware.com>
2023-09-06 00:37:02 +0800

    add ononce hook for reloading styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { API } from '@stoplight/elements';
 import { subscribeTheme } from "@stoplight/mosaic";
-import { useCallback, useEffect } from 'react';
+import { useCallback, useLayoutEffect } from 'react';
 import './App.scss';
 import { Header } from './Header';
 import { ScrollContainer } from './ScrollContainer';
@@ -11,14 +11,14 @@ function App() {
     () => {
       const overrideStyle = document.createElement('link');
       overrideStyle.rel = 'stylesheet';
-      overrideStyle.href = 'stoplight.css';
+      overrideStyle.href = `${process.env.PUBLIC_URL}/stoplight.css`
       overrideStyle.type = 'text/css';
       document.head.appendChild(overrideStyle)
     },
     [],
   );
 
-  useEffect(
+  useLayoutEffect(
     () => {
       subscribeTheme({ mode: 'dark' })
       const elem = document.querySelector("#mosaic-provider-react-aria-0-2 > div > div > div > div.sl-flex > div.sl-flex.sl-overflow-y-auto.sl-flex-col.sl-sticky.sl-inset-y-0.sl-pt-8.sl-bg-canvas-100.sl-border-r > a")
